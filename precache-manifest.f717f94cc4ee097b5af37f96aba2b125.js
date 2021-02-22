7ed192c41f3cd087d189df3ebc5f452109275fa6
YannVoytaa <wojtachjanek@gmail.com>
2021-02-22 15:00:36 +0100

    Updates

---
+++ precache-manifest.f717f94cc4ee097b5af37f96aba2b125.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4b079968427791efff2c4b11bcfebe",
+    "url": "/react_doodler/index.html"
+  },
+  {
+    "revision": "afc9513956452814a226",
+    "url": "/react_doodler/static/css/main.cf0e2f67.chunk.css"
+  },
+  {
+    "revision": "894c493f94702008de88",
+    "url": "/react_doodler/static/js/2.38fc4214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_doodler/static/js/2.38fc4214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc9513956452814a226",
+    "url": "/react_doodler/static/js/main.e4a8157c.chunk.js"
+  },
+  {
+    "revision": "d13985513297b76bcdcf",
+    "url": "/react_doodler/static/js/runtime-main.10299510.js"
+  },
+  {
+    "revision": "b1b0ee7165ed59c8de4d7dc940bb8c44",
+    "url": "/react_doodler/static/media/bg.b1b0ee71.png"
+  },
+  {
+    "revision": "cfd3b6298ef380c23fc09f7352c921d4",
+    "url": "/react_doodler/static/media/doodlert.cfd3b629.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_doodler/static/media/logo.5d5d9eef.svg"
+  }
+]);
